198cdd91edd99d6370defddd8a32e58ee76b7362
riya2509 <riyamar3@gmail.com>
2021-10-15 10:59:38 -0400

    exercise1.1

---
+++ exc1.1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+    int a, b, c;
+    printf ("Enter three integers\n");
+    scanf ("%d%d%d", &a, &b, &c);
+    printf("First=%d\n",a);
+    printf("Second=%d\n",b);
+    printf("Third=%d\n",c);
+}
