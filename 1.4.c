5c031d15f7c90c473ef25968f3f7ee6050654031
riya2509 <riyamar3@gmail.com>
2021-10-15 12:01:29 -0400

    ans1.4

---
+++ 1.4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PI 3.14159
+void main(){
+    int radius;
+    int area;
+    printf("Enter the radius of a circle\n");
+    scanf("%d",&radius);
+    area=PI*radius*radius;
+    printf("The area of the circle is=%d",area);
+
+
+
+
+}
