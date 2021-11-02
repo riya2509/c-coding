2ab030084d669fc1ec009197ca3dfa5efe7cac08
riya2509 <91462077+riya2509@users.noreply.github.com>
2021-11-02 04:31:11 -0400

    Add files via upload
    printing Fibonacci series

---
+++ cse lab/5Fibonacciseries.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+     int n1=0,n2=1,n3,i,number;    
+     printf("Enter the number of elements:");    
+     scanf("%d",&number);    
+     printf("\n%d",n2);//printing 1    
+     for(i=2;i<number;++i)//loop starts from 2 because 1 is already printed    
+     {    
+      n3=n1+n2;    
+      printf(" %d",n3);    
+      n1=n2;    
+      n2=n3;    
+     }  
+
+}    
