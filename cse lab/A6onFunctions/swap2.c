b2d08779aecb22e47e8ce10e0899275c52627131
riya2509 <riyamars3@gmail.com>
2021-12-13 15:50:48 +0530

    swapping 2 numbers using function

---
+++ cse lab/A6onFunctions/swap2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void swap(int *,int *);
+int main()
+{
+int n1,n2;   
+printf("Input 1st number : ");
+scanf("%d",&n1);
+printf("Input 2nd number : ");
+scanf("%d",&n2);	
+printf("Before swapping: n1 = %d, n2 = %d ",n1,n2);
+swap(&n1,&n2);
+printf("\nAfter swapping: n1 = %d, n2 = %d \n\n",n1,n2);
+return 0;
+}
+void swap(int *p,int *q)
+{
+//p=&n1 *p=n1
+//q=&n2 *q=n2
+int tmp;
+tmp = *p; // tmp store the value of n1
+*p=*q;    // *p store the value of *q that is value of n2
+*q=tmp;   // *q store the value of tmp that is the value of n1
+}
