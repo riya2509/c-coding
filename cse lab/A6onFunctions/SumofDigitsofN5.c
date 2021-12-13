4268f2d4eee12e663b05ca9ae98cd03e95a8669b
riya2509 <riyamars3@gmail.com>
2021-12-13 16:12:08 +0530

    Sum of digits of a number n

---
+++ cse lab/A6onFunctions/SumofDigitsofN5.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int DigitSum(int num); 
+int main()
+{
+int n1, sum;
+printf(" Input any number to find sum of digits: ");
+scanf("%d", &n1);     
+sum = DigitSum(n1);//calling the function for calculation     
+printf(" The Sum of digits of %d = %d\n\n", n1, sum);     
+return 0;
+}
+int DigitSum(int n1)
+{
+if(n1 == 0)
+return 0;         
+return ((n1 % 10) + DigitSum(n1 / 10));//calling the function DigitSum itself
+}
