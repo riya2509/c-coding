f8d3be52d72ff33d565bc03e0461c912219b0070
riya2509 <riyamars3@gmail.com>
2021-12-14 15:56:17 +0530

    changed

--- cse lab/Test3 14th Dec/ans2.c
+++ cse lab/Test3 14th Dec/ans2.c
@@ -1,32 +1,30 @@
 
 #include <stdio.h>
-
 int main(){
-    int m,n,i,j,a[10][10];
-    printf("Enter the no. of rows: ");
-    scanf("%d",&m);
-    printf("Enter the no. of columns: ");
-    scanf("%d",&n);
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            printf("Enter number: ");
-            scanf("%d",&a[i][j]);
-        }
-    }
-    printf("\n");
-    for(i=0;i<m;i++){
-        for(j=n-1;j>=0;j--){
-            printf("%d ",a[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            printf("%d ",a[i][j]);
-        }
-        printf("\n");
-    }
-    
-    return 0;
+int m,n,i,j,a[10][10];
+printf("Enter the no. of rows: ");
+scanf("%d",&m);
+printf("Enter the no. of columns: ");
+scanf("%d",&n);
+for(i=0;i<m;i++){
+for(j=0;j<n;j++){
+printf("Enter number: ");
+scanf("%d",&a[i][j]);
+}
+}
+printf("\n");
+for(i=0;i<m;i++){
+for(j=n-1;j>=0;j--){
+printf("%d ",a[i][j]);
+}
+printf("\n");
+}
+printf("\n");
+for(i=0;i<m;i++){
+for(j=0;j<n;j++){
+printf("%d ",a[i][j]);
+}
+printf("\n");
+}    
+return 0;
 }
