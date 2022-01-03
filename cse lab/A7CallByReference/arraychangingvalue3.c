735013a759c7117a4c52d8635420124f1551f8f1
riya2509 <riyamars3@gmail.com>
2022-01-03 17:13:45 +0530

    array changing value

---
+++ cse lab/A7CallByReference/arraychangingvalue3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int* change( int *p2, int a[])
+{
+    int *p;
+    p=&a[0]; int temp=0;
+    while(p<=&a[2])
+    {
+        if(p==&a[1])
+        {
+            temp= *p;
+            *p= *p2;
+            *p2=temp;
+            
+        }
+        p=p+1;
+    }
+    return a;
+}
+void main()
+{
+    int a[3];
+    a[0]=10;
+    a[1]=20;
+    a[2]=30; int num=200;
+    change(&num, a);
+    for(int i=0;i<3;i++)
+    printf("%d\t",a[i]);
+}
