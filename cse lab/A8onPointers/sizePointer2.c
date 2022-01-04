33848e463f519d33bec21fd61beef11b72f90fe7
riya2509 <riyamars3@gmail.com>
2022-01-04 15:01:30 +0530

    Calculating size of pointers

---
+++ cse lab/A8onPointers/sizePointer2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("\nsize of long int pointer: %d" ,sizeof(long int*));
+    printf("\nsize of int pointer: %d"      ,sizeof(int*));
+    printf("\nsize of char pointer: %d"     ,sizeof(char*));
+    printf("\nsize of double pointer: %d\n" ,sizeof(double*));
+    printf("\nsize of float pointer: %d"    ,sizeof(float*));
+    return 0;
+}
