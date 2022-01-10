161837a52251890efc70c0a8ce67c9fa84071400
riya2509 <riyamars3@gmail.com>
2022-01-10 16:34:40 +0530

    calculating the number of Vowels nad consonants

---
+++ cse lab/A9Strings/NoOfVowels&Cons7.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+void main()
+{
+char s[80];
+int i, vowels = 0, consonants = 0, special = 0; 
+printf("Enter a sentence \n");
+gets(s);
+for (i = 0; s[i] != '\0'; i++)
+{
+if ((s[i] == 'a' || s[i] == 'e' || s[i] =='i' || s[i] == 'o' || s[i] == 'u') ||(s[i] == 'A' || s[i] == 'E' || s[i] =='I' || s[i] == 'O' || s[i] == 'U'))
+{
+vowels = vowels + 1;
+}
+else
+{
+consonants = consonants + 1;
+}
+if (s[i] =='\t' ||s[i] =='\0' || s[i] ==' ')
+{
+special = special + 1;
+}
+}
+consonants = consonants - special;
+printf("No. of vowels in %s = %d\n", s, vowels);
+printf("No. of consonants in %s = %d\n", s, consonants);
+}
