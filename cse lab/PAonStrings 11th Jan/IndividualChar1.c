9bed9a18ca6f5ba0504dea280dca13e11d2326ab
riya2509 <riyamars3@gmail.com>
2022-01-11 16:59:29 +0530

    spacing the individual characters from a string

---
+++ cse lab/PAonStrings 11th Jan/IndividualChar1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+char str[100]; 
+int l= 0;	
+printf("Input the string : ");
+gets(str);
+printf("The characters of the string are : \n");
+while(str[l]!='\0')
+{
+printf("%c  ", str[l]);
+l++;
+}
+printf("\n");
+}
