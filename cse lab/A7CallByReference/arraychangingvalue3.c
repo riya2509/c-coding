14d50c80412266ada3af7c9cc6ecf971b3fb5d8f
riya2509 <riyamars3@gmail.com>
2022-02-13 13:08:12 +0530

    array changing values modified

--- cse lab/A7CallByReference/arraychangingvalue3.c
+++ cse lab/A7CallByReference/arraychangingvalue3.c
@@ -19,9 +19,9 @@ int* change( int *p2, int a[])
 void main()
 {
     int a[3];
-    a[0]=10;
+    a[0]=100;
     a[1]=20;
-    a[2]=30; int num=200;
+    a[2]=60; int num=800;
     change(&num, a);
     for(int i=0;i<3;i++)
     printf("%d\t",a[i]);
