8de7fb56dd94719962b905047fec9f521e80052c
riya2509 <riyamars3@gmail.com>
2022-07-25 12:59:47 +0530

    reversing the array

---
+++ DSALab/Lab2/2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+    int n, i, arr1[15];
+    int *pt;
+    printf(" Input the number of elements to store in the array (max 15) : ");
+    scanf("%d", &n);
+    pt = &arr1[0]; // pt storing the address of base array arr1
+    printf(" Input %d number of elements in the array : \n", n);
+    for (i = 0; i < n; i++)
+    {
+        printf(" element - %d : ", i + 1);
+        scanf("%d", pt); // accepting the address of the value
+        pt++;
+    }
+    pt = &arr1[n - 1];
+    printf("\n The elements of array in reverse order are :");
+    for (i = n; i > 0; i--)
+    {
+        printf("\n element %d : %d  ", i, *pt);
+        pt--;
+    }
+    printf("\n\n");
+}
