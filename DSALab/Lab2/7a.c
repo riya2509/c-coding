24c5674b9097b5b79cd2a7fd427457918b6aa4a0
riya2509 <riyamars3@gmail.com>
2022-07-31 12:07:23 +0530

    distance between two points using DMA

---
+++ DSALab/Lab2/7a.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <math.h>
+#include<stdlib.h>
+struct point {
+    float x;
+    float y;
+};
+void main(){
+ struct point a,b;
+    struct point *ptr;
+    float d;
+    int noOfRecords;
+  printf("Enter the number of records: ");
+  scanf("%d", &noOfRecords);
+   ptr = (struct point *)malloc(noOfRecords * sizeof(struct point));
+  for (int i = 0; i < noOfRecords; ++i){
+
+  
+    printf("Enter the first point :");
+    scanf("%f %f",&a.x, &a.y);
+    printf("Enter the second point :");
+    scanf("%f %f",&b.x, &b.y);
+  }
+    d= sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
+    printf("The distance between the two points is:%f",d);
+    free(ptr);
+}
