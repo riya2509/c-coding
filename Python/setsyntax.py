4bbbf0fe350181c86cd792df903eb863352c2420
riya2509 <riyamars3@gmail.com>
2022-08-03 12:51:41 +0530

    learning sets

---
+++ Python/setsyntax.py
@@ -0,0 +1,12 @@
+set_var={'Riya','huedfkl','uiehf','euiuierh'}
+set1= {"riya",'girl','a'}
+set2= {'riya','you','are','a','girl'}
+print(set_var)
+print(type(set_var))
+#We cannot find out indexing in sets
+print(set_var.add('fguoufg'))
+#difference
+print(set2.difference(set1))
+print(set2)
+## updating the set 2
+print(set2.difference_update(set1))
