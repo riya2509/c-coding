3791b098ac77e5b8a28adabb3526632dca9775d3
riya2509 <riyamars3@gmail.com>
2022-08-04 12:12:54 +0530

    sets practise python

--- Python/setsyntax.py
+++ Python/setsyntax.py
@@ -4,9 +4,11 @@ set2= {'riya','you','are','a','girl'}
 print(set_var)
 print(type(set_var))
 #We cannot find out indexing in sets
-print(set_var.add('fguoufg'))
+set_var.add('fguoufg')
+print(set_var)
 #difference
 print(set2.difference(set1))
 print(set2)
 ## updating the set 2
-print(set2.difference_update(set1))
+set2.difference_update(set1)
+print(set2)
